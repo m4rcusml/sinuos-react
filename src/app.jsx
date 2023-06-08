24b69a1301fbdf75e683dc30a006ca2330cd2e15
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2023-06-08 16:57:33 -0400

    Initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+import './App.css'
 
-import Header from './Header'
-import Menu from './Menu'
-import Footer from './Footer'
-import Inicio from './Inicio'
-import Desenvolvedores from './Desenvolvedores'
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Main />
+    </>
+  )
+}
 
-import LogoBanner from './components/LogoBanner'
-
-import './app.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Header />
-    <Menu />
-    <main>
-      <LogoBanner/>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Inicio />}></Route>
-          <Route path='/desenvolvedores' element={<Desenvolvedores />}></Route>
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </main>
-  </React.StrictMode>,
-)
+export default App
